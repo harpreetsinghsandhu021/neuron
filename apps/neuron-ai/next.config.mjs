dd4322cc83403ce1562107082faad8676cb0da00
Harpreet Singh <harpreetsinghsandhu021@gmail.com>
2024-07-29 08:43:41 +0530

    fix - prisma init eror

--- apps/neuron-ai/next.config.mjs
+++ apps/neuron-ai/next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
   images: {
     remotePatterns: [
       {
